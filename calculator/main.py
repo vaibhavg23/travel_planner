74e8c4c97190be84d2410876102651c67720a9a1
vaibhavg23 <155292437+vaibhavg23@users.noreply.github.com>
2025-01-30 21:04:01 +0530

    Delete calculator directory

--- calculator/main.py
+++
@@ -1,8 +0,0 @@
-a=40
-b=6
-print("The value of", a,"+",b, "is:", a+b)
-print("The value of", a,"-",b, "is:", a-b)
-print("The value of", a,"*",b, "is:", a*b)
-print("The value of", a,"//",b, "is:", a//b)
-print("The value of", a,"/",b, "is:", a/b)
-print("The value of", a,"%",b, "is:", a%b)
